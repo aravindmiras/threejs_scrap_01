7bf0019fdde483669a195d7f5a550b8fc0be75b9
Usha Kalathinathan <ushasasi7018@gmail.com>
2023-12-09 16:18:25 +0530

    add webxr controls

--- src/Xrcube.jsx
+++ src/Xrcube.jsx
@@ -4,6 +4,7 @@ import Loader from './components/Loader.jsx'
 import Island from './models/Island.jsx'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import XrHitModel from './models/Container.jsx'
 
 
 const XrCube = () => {
@@ -11,8 +12,12 @@ const XrCube = () => {
     const [isRotating, setIsRotating] = useState(false)
     return (
         <section className="w-full h-screen relative">
-        <ARButton/>
+            <div className="w-full h-screen relative z-0">
+                
+        <div className="absolute inset-0 flex"> 
+        <ARButton position={[1,2,4]}/>       
         
+        </div>
         <Canvas
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{near: 0.1, far:1000}}
@@ -32,9 +37,11 @@ const XrCube = () => {
             maxPolarAngle={Math.PI}
             minPolarAngle={0}
             /> */}
-            <Island/>
+            <XrHitModel/>
             </XR>
         </Canvas>
+        
+        </div>
     </section>
     );
 };
